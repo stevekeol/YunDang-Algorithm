7852ba5e5b5826c68414cdcfd0d7f07a10aab8f0
stevekeol <stevekeol.x@gmail.com>
2021-04-13 19:43:27 +0800

    :beers: task-scheduler # 621

--- private/Leetcode/task-scheduler.js
+++ private/Leetcode/task-scheduler.js
@@ -30,52 +30,104 @@
  * 性能：时间O(n), 空间O(1)
  ************************************************************/
 
-const tasks = ['A', 'B', 'A', 'A', 'B', 'A', 'B', 'B', 'D', 'D', 'E', 'C', 'C', 'C'];
+const tasks = ['A', 'B', 'A', 'A', 'A', 'B', 'A', 'B', 'B', 'D', 'D', 'E', 'C', 'C', 'C'];
 const n = 2;
 
-const leastInterval = function(tasks, n) {
-  if(n == 0) {
-    return tasks.length;
-  }
+// const leastInterval = function(tasks, n) {
+//   if(n == 0) {
+//     return tasks.length;
+//   }
 
-  let length = 0;
-  let { map, arr } = test(tasks);
-  while(arr.length > 0) {
-    if(arr.length >= (n + 1)) {
-      length += arr[n][1] * (n + 1);
-      map.delete(arr[n][0]);
-    } else {
-      
-    }
+//   let length = 0;
+//   let { map, arr } = test(tasks);
+//   while(arr.length > 0) {
+//     if(arr.length >= (n + 1)) {
+//       length += arr[n][1] * (n + 1);
+//       map.delete(arr[n][0]);
+//       for(let i = 0; i < n + 1; i ++) {
+//         arr[i][1] = arr[i][i] - arr[n][1];
+//       }
+//     } else {
+//       while(n > 1) {
+
+//       }
+//     }
 
 
-    // if(arr.length == n) {
-    //   length += arr[n - 1][1] * (n + 1);
-    //   map.delete(arr[n - 1][0]);
-    // } else if((n - 1) > 0 && arr.length == (n - 1)) {
-    //   length += arr[n - 2][1] * (n + 1);
-    //   map.delete(arr[n - 2][0]);
-    // }
-  }
+//     // if(arr.length == n) {
+//     //   length += arr[n - 1][1] * (n + 1);
+//     //   map.delete(arr[n - 1][0]);
+//     // } else if((n - 1) > 0 && arr.length == (n - 1)) {
+//     //   length += arr[n - 2][1] * (n + 1);
+//     //   map.delete(arr[n - 2][0]);
+//     // }
+//   }
+// };
+
+// const test = tasks => {
+//   let map = new Map();
+//   tasks.map(task => {
+//     if(map.get(task)) {
+//       let newValue = map.get(task) + 1
+//       map.set(task, newValue);
+//     } else {
+//       map.set(task, 1);
+//     }
+//   })
+//   console.log(map)
+//   let arr = [...map];
+
+//   arr.sort((x, y) => y[1] - x[1]);
+//   console.table(arr);
+//   return {map, arr};
+// }
+
+// console.log(test(tasks))
+
+
+// var leastInterval = function(tasks, n) {
+//     const freq = _.countBy(tasks);
+
+//     // 任务总数
+//     const m = Object.keys(freq).length;
+//     const nextValid = new Array(m).fill(1);
+//     const rest = Object.values(freq);
+
+//     let time = 0;
+//     for (let i = 0; i < tasks.length; i++) {
+//         time++;
+//         let minNextValid = Number.MAX_VALUE;
+//         for (let j = 0; j < m; j++) {
+//             if (rest[j] > 0) {
+//                 minNextValid = Math.min(nextValid[j], minNextValid);
+//             }
+//         }
+//         time = Math.max(time, minNextValid);
+
+//         let best = -1;
+//         for (let j = 0; j < m; j++) {
+//             if (rest[j] && nextValid[j] <= time) {
+//                 if (best === -1 || rest[j] > rest[best]) {
+//                     best = j;
+//                 }
+//             }
+//         }
+
+//         nextValid[best] = time + n + 1;
+//         rest[best]--;
+//     }
+
+//     return time;
+// };
+
+var leastInterval = function(tasks, n, h = Array(26).fill(0)) {
+    if (n === 0) return tasks.length
+    for(var i = 0; i < tasks.length; i++) 
+        h[tasks[i].charCodeAt() - 65]++
+    var max = Math.max(...h), maxCount = 0
+    console.log(max);
+    h.forEach(n => n === max && maxCount++)
+    return Math.max((max - 1) * (n + 1) + maxCount, tasks.length)
 };
 
-const test = tasks => {
-  let map = new Map();
-  tasks.map(task => {
-    if(map.get(task)) {
-      let newValue = map.get(task) + 1
-      map.set(task, newValue);
-    } else {
-      map.set(task, 1);
-    }
-  })
-  console.log(map)
-  let arr = [...map];
-
-  arr.sort((x, y) => y[1] - x[1]);
-  console.table(arr);
-  return {map, arr};
-}
-
-console.log(test(tasks))
-
+console.log(leastInterval(tasks, n));
