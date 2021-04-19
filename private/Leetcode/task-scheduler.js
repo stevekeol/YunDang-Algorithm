6d1f1cfe8bb628428e71e825000f7769e0287440
stevekeol <stevekeol.x@gmail.com>
2021-04-19 14:25:31 +0800

    :bug: task-shceduler.js

--- private/Leetcode/task-scheduler.js
+++ private/Leetcode/task-scheduler.js
@@ -39,7 +39,8 @@ const leastInterval = (tasks, n, h = Array(26).fill(0)) => {
   if (n === 0) return tasks.length
   for(let i = 0; i < tasks.length; i++) 
     h[tasks[i].charCodeAt() - 65]++
-  const max = Math.max(...h), maxCount = 0 // 擅于利用已有工具!
+  const max = Math.max(...h);
+  let maxCount = 0;
   h.forEach(n => n === max && maxCount++);
   return Math.max((max - 1) * (n + 1) + maxCount, tasks.length);
 };
